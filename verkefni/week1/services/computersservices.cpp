565710800cc7ef5e16c84bf1072e68895bc2f619
svavar15 <svavar15@ru.is>
2015-12-04 14:46:56 +0000

    New In

---
+++ verkefni/week1/services/computersservices.cpp
@@ -0,0 +1,7 @@
+#include "computerservices.h"
+
+computerservices::computerservices()
+{
+
+}
+
---
+++ verkefni/week1/services/computersservices.h
@@ -0,0 +1,11 @@
+#ifndef COMPUTERSERVICES_H
+#define COMPUTERSERVICES_H
+
+
+class computerservices
+{
+public:
+    computerservices();
+};
+
+#endif // COMPUTERSERVICES_H
