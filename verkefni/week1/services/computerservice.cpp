07648aa25732400a97a31480f7ca82f30dbba078
haukurig15 <haukurig15@ru.is>
2015-12-04 13:58:40 +0000

    services filear

---
+++ verkefni/week1/services/computerservice.cpp
@@ -0,0 +1,7 @@
+#include "computerservice.h"
+
+computerservice::computerservice()
+{
+
+}
+
---
+++ verkefni/week1/services/computerservice.h
@@ -0,0 +1,11 @@
+#ifndef COMPUTERSERVICE_H
+#define COMPUTERSERVICE_H
+
+
+class computerservice
+{
+public:
+    computerservice();
+};
+
+#endif // COMPUTERSERVICE_H
