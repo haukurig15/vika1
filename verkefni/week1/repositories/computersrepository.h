b8a3223833c58910506f67bbb016eec41b59128a
svavar15 <svavar15@ru.is>
2015-12-04 13:25:54 +0000

    Virkar þetta?

--- verkefni/week1/repositories/computersrepository.h
+++ verkefni/week1/repositories/computersrepository.h
@@ -1,11 +1,42 @@
 #ifndef COMPUTERSREPOSITORY_H
 #define COMPUTERSREPOSITORY_H
 
+#include <vector>
+#include <string>
 
-class computersrepository
+#include "models/computers.h"
+
+
+/**
+ * @brief The ScientistRepository class handles reading and writing scientists to and from a file
+ */
+class ComputersRepository
 {
 public:
-    computersrepository();
+    ComputersRepository();
+
+    /**
+     * @brief getAllComputers fetches all scientists from the data file
+     * @return a vector containing all scientists in the data file
+     */
+    std::vector<Computers> getAllComputers();
+
+    /**
+     * @brief searchForComputers fetches all scientists from file and filters them on searchTerm
+     * @param searchTerm Contains the input that the user wishes to filter on
+     * @return a vector of scientists filtered by searchTerm
+     */
+    std::vector<Computers> searchForComputers(std::string searchTerm);
+
+    /**
+     * @brief addComputers saves a scientist model to a file
+     * @param scientist The model to save
+     * @return true if it was a success, false if it was a failure
+     */
+    bool addComputers(Computers computers);
+
+private:
+    std::string fileName;
 };
 
 #endif // COMPUTERSREPOSITORY_H
